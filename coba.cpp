3d78a8764af70e1262742591b580db21ff443852
IkhsaanHanafi <ikhsaanhanafi@gmail.com>
2025-04-28 18:20:18 +0700

    display data

--- coba.cpp
+++ coba.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
 using namespace std; 
+
+class Planet {
+    private :
+       float gravitasi;
+    public : 
+       string nama;
+       float diameter;
+
+       void displaydata(){
+        cout << "Halo aku planet" << nama << endl;
+        cout << "ukuranku" << diameter << endl;
+        cout << "gravitasiku" << gravitasi << endl;
+       }
+
+
+};

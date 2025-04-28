102d14bd4d6b60feebf9cc43fd75a365314b95b3
IkhsaanHanafi <ikhsaanhanafi@gmail.com>
2025-04-28 19:15:56 +0700

    persegi

---
+++ bebas.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class persegipanjang {
+    public :
+        float panjang;
+        float lebar;
+        float luas;
+        void inputData(){
+            cout << "input panjang : ";
+            cin >> panjang;
+            cout << "input lebar : ";
+            cin >> lebar;
+        }
+
+        void outputData(){
+            
+        }
+
+        void menghitungluas(){
+            luas = panjang*lebar;
+            cout << "luas persegi panjang = " << luas << endl;
+
+        }
+};
+
+int main()
+{
+    persegipanjang per ;
+    per.inputData();
+    per.menghitungluas();
+    return 0;
+
+};
